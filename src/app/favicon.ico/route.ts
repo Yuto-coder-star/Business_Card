6f15e958f5fab9d287f5478df2c2cb4792350c8b
Yuto-coder-star <tsuruta@marutto.jp>
2025-10-08 22:17:10 +0900

    Delete src directory

--- src/app/favicon.ico/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export const runtime = "edge";
-
-export function GET(request: Request) {
-  const redirectUrl = new URL("/icon.svg", request.url);
-  return NextResponse.redirect(redirectUrl, 308);
-}
